d337fd9460e8b7652437fd0cdcfe5ca05225b8ae
Never-seen <72398213+Never-seen@users.noreply.github.com>
2020-11-16 11:12:34 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,65 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include "Menu.h"
+
+using namespace sf;
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML WORK!");
+
+	Menu menu(600, 600);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					menu.MoveUp();
+					break;
+
+				case sf::Keyboard::Down:
+					menu.MoveDown();
+					break;
+
+				case sf::Keyboard::Return:
+					switch (menu.GetPressedItem())
+					{
+					case 0:
+						std::cout << "Play button has been pressed" << std::endl;
+						break;
+					case 1:
+						std::cout << "Tutorial button has been pressed" << std::endl;
+						break;
+					case 2:
+						window.close();
+						break;
+					}
+
+					break;
+				}
+
+				break;
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			}
+		}
+
+		window.clear();
+
+		menu.draw(window);
+
+		window.display();
+	}
+}
